4ab6bd3d9ac8f797c7657513affcc5c53d6a32c2
Chenyu Shen Rainy <34141116+surancy@users.noreply.github.com>
2019-04-20 19:40:08 -0400

    Update Answering
    - Made the "who" answer more legit (i.e. a full sentence instead of a single word)
    - Updated the "when" answer criteria
    
    Planned next steps:
    - Refine the answer for remaining question types (made them into a full sentence rather than one word)
    - Coming up with good strategies for more descriptive questions (i.e. reason, description, these kinds of Q)

--- answer.py
+++ answer.py
@@ -1,29 +1,39 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[17]:
-
+"""
+usage: text is the text of which can be extracted answers from. Q is the question.
+Some test cases are included at the bottom.
+====In progress===
+"""
 
 import spacy
 import en_core_web_sm
 nlp = en_core_web_sm.load()
 
-from textblob import TextBlob
-
-
-# In[30]:
-
+from textblob import TextBlob # used for sentiment analysis
 
 def ans(text,Q):
+    # parse the Q with spacy parsing
+    Q = nlp(Q)
     """
     for who questions, look for phrases with the NER tags: PERSON, ORG, in the text that contains answer.
     only look for phrases where proper noun is the subject within sentence
-    
-    if no NER tag, look for similarities of word2vec
     """
-
-    
+    """
+    for who questions, look for phrases with the NER tags: [PERSON || ORG] in the text that contains answer.
+    only look for phrases where proper noun is the subject within sentence
+    """
     if (str(Q[0]).lower() == "who"):
+        # preprocess the Question
+        cleanQ = list(Q)
+        ## get rid of question mark at the end, if any
+        cleanQ = [str(words) for words in cleanQ if str(words) != "?"]
+        ## start from 1 to get rid of "who"
+        ## fix the negation tokenization
+        for i,token in enumerate(cleanQ):
+            if token == "'s":
+                cleanQ.remove(token)
+                cleanQ[i - 1] = cleanQ[i - 1] + token
+        strQbody = " ".join(cleanQ[i] for i in range(1, len(cleanQ)))
+        
         text = nlp(text)
         ans = ""
         ent_dict = dict()
@@ -33,12 +43,10 @@ def ans(text,Q):
             for k,v in ent_dict.items():
                 if (token.dep_ == "nsubj"):
                     if (v == "PERSON" or v == "ORG"):
-                        ans = token.text
+                        ans = token.text + " " + strQbody +"."
     
     """
-    for when questions, look for phrases with the NER tags: DATE, in the text that contains answer.
-    
-    backup: 
+    for when questions, look for phrases with the NER tags: [DATE || TIME || CARDINAL] from the text that contains answer.
     """
     if (str(Q[0]).lower() == "when"):
         text = nlp(text)
@@ -47,8 +55,8 @@ def ans(text,Q):
         for X in text.ents:
             ent_dict[X.text] = X.label_
         for k,v in ent_dict.items():
-            if (v == "DATE"):
-                ans = k
+            if (v == "DATE" or v == "CARDINAL" or v == "TIME"):
+                
     """
     for where questions, look for phrases with the NER tags: GPE, LOC, in the text that contains answer.
     """
@@ -89,27 +97,31 @@ def ans(text,Q):
     return ans
 
 
-# In[31]:
+# ===========Testing============= #
+## WHO
+text = "Queen Hatshepsut concentrated on expanding Egypt's external trade by sending a commercial expedition to the land of Punt."
+Q = "who concentrated on expanding Egypt's external trade?"
+### ans: "Hatshepsut concentrated on expanding Egypt's external trade."
+
+## WHEN
+text = ('The Old Kingdom is the period in'
+        'the third millennium (c. 2686-2181 BC)'
+        'also known as the \'Age of the Pyramids\' or \'Age of the Pyramid Builders\''
+        'as it includes the great 4th Dynasty when King Sneferu perfected the art of pyramid building'
+        'and the pyramids of Giza were constructed under the kings Khufu, Khafre, and Menkaure.')
+Q = "when is the Old Kingdom?"
+### ans: '2686-2181'
+
+text = "During the reign of Thutmose III (c. 1479–1425 BC), Pharaoh, originally referring to the king's palace, became a form of address for the person who was king"
+Q = "when is the reign of Thutmose?"
+### ans: '1479–1425'
 
 
-# testing
-# text = nlp("Under King Djoser, the first king of the Third Dynasty of the Old Kingdom the royal capital of Egypt was moved to Memphis, where Djoser established his court.")
-# Q = nlp("who is the first king of the Third Dynasty of the Old Kingdom?")
-# Q = nlp("where does the Old Kingdom the royal capital of Egypt moved to?")
-
-text = "The Old Kingdom is the period in the third millennium (c. 2686-2181 BC) also known as the \'Age of the Pyramids\' or \'Age of the Pyramid Builders\' as it includes the great 4th Dynasty when King Sneferu perfected the art of pyramid building and the pyramids of Giza were constructed under the kings Khufu, Khafre, and Menkaure. "
-# Q = nlp("who perfected the art of pyramid building?")
-# Q = nlp("when is the Old Kingdom?")
-Q = nlp("Is the Old Kingdom also known as the \'Age of the Pyramids\' or \'Age of the Pyramid Builders\'?")
-
-
-
+text = ("The severity of the difficulties is indicated by the fact that the first known labor strike" 
+        "in recorded history occurred during the 29th year of Ramesses III's reign, when the food rations for Egypt's favored"
+        "and elite royal tomb-builders and artisans in the village of Deir el Medina could not be provisioned.")
+Q = "when is the Ramesses's reign?"
+### ans: 'the 29th year'
 
 ans(text,Q)
-
-
-# In[ ]:
-
-
-
 
